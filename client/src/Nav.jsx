d8e02a2513fee75ee724bfa26e04683025609515
iMrihan <mhd.khan.rihan@gmail.com>
2022-08-03 13:34:37 +0530

    routers added

---
+++ client/src/Nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div>
+      <ul className="nav-ul">
+        <li>
+          <Link to="/">Products</Link>
+          <Link to="/add">Add Product</Link>
+          <Link to="/update">Update Product</Link>
+          <Link to="/logout">Logout</Link>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
