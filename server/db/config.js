e9aa060a35e83892b115ba97e8fca29f93caed87
iMrihan <mhd.khan.rihan@gmail.com>
2022-08-05 20:14:22 +0530

    conncet database with mongodb atlas

---
+++ server/db/config.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://rihanecom:rihanecom@cluster0.efp1xwe.mongodb.net/?retryWrites=true&w=majority"
+);
--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("app is working...");
+const cors = request("cors");
+require("./db/config");
+const User = require("./db/User");
+app.use(express.json());
+app.use(cors());
+app.post("/register", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  res.send(result);
 });
 
 app.listen(3005, () => {
