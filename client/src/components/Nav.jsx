abf625206d35b91f271ec576cd8dd58d710591ee
iMrihan <mhd.khan.rihan@gmail.com>
2022-08-07 17:57:53 +0530

    login component is working now

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -10,39 +10,44 @@ const Nav = () => {
   };
 
   return (
-    <div>
-      <ul className="nav-ul">
-        <li>
-          <Link to="/">Products</Link>
-        </li>
-        <li>
-          <Link to="/add">Add Product</Link>
-        </li>
-        <li>
-          <Link to="/update">Update Product</Link>
-        </li>
+    <div className="nav">
+      <img
+        className="logo"
+        src="https://images-platform.99static.com/RtnLnvmEEPYFRW6Y9z2c_sScoTU=/18x1563:1533x3078/500x500/top/smart/99designs-contests-attachments/96/96847/attachment_96847724"
+        alt="logo"
+      />
 
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Product</Link>
+          </li>
+          <li>
+            <Link to="/update">Update Product</Link>
+          </li>
 
-        {auth ? (
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
           <li>
             <Link onClick={logout} to="/signup">
-              Logout
+              Logout ( {JSON.parse(auth).name})
             </Link>
           </li>
-        ) : (
-          <>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </>
-        )}
-      </ul>
+        </ul>
+      ) : (
+        <ul className="nav-ul nav-right">
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
