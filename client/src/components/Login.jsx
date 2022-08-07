4893b8842850d85411ba5130246dd774f1dfeceb
iMrihan <mhd.khan.rihan@gmail.com>
2022-08-07 17:11:19 +0530

    login component is working now

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
@@ -14,6 +14,13 @@ export default function Login() {
     const { name, value } = e.target;
     setLoginData({ ...loginData, [name]: value });
   };
+
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
