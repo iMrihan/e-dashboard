29931bfdf9d253660c73f00b0b81d61405c572b5
iMrihan <mhd.khan.rihan@gmail.com>
2022-08-10 19:29:35 +0530

    search funtionallity is added

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -25,10 +25,29 @@ export default function ProductList() {
     }
   };
 
+  const searchHandle = async (event) => {
+    let key = event.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:3005/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
+    }
+  };
+
   console.log(products);
   return (
     <div className="products-list">
       <h3>Product List</h3>
+      <input
+        type="text"
+        onChange={searchHandle}
+        className="search-product-box"
+        placeholder="Search Product"
+      />
       <ul>
         <li>S.No</li>
         <li>Name</li>
@@ -39,22 +58,26 @@ export default function ProductList() {
         <li>Operation-2</li>
       </ul>
 
-      {products.map((el, index) => (
-        <ul key={el._id}>
-          <li>{index + 1}</li>
-          <li>{el.name}</li>
-          <li>{el.price}</li>
-          <li>{el.category}</li>
-          <li>{el.company}</li>
-          <li>
-            {" "}
-            <button onClick={() => deleteProduct(el._id)}>Delete</button>
-          </li>
-          <li>
-            <Link to={`/update/${el._id}`}>Update</Link>
-          </li>
-        </ul>
-      ))}
+      {products.length > 0 ? (
+        products.map((el, index) => (
+          <ul key={el._id}>
+            <li>{index + 1}</li>
+            <li>{el.name}</li>
+            <li>{el.price}</li>
+            <li>{el.category}</li>
+            <li>{el.company}</li>
+            <li>
+              {" "}
+              <button onClick={() => deleteProduct(el._id)}>Delete</button>
+            </li>
+            <li>
+              <Link to={`/update/${el._id}`}>Update</Link>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No Result Found</h1>
+      )}
     </div>
   );
 }
